638f3cf8712cfcca77a07ddb207f5ceb147eb9e4
Andrew Bennett <awbennett@wisc.edu>
2026-06-12 12:15:08 -0700

    start explosion work

--- scripts/planet.js
+++ scripts/planet.js
@@ -1,8 +1,13 @@
 import * as THREE from 'three';
 import { TITLE_FADE_START, TITLE_FADE_END } from './constants.js';
 
+const ROCK_COUNT = 8;
+const ROCK_SIZE = 0.3;
+const ROCK_VARIANCE = 0.25;
+
 const PLANET_RADIUS = 50;
-const TREE_COUNT = 15;
+const TREE_COUNT = 16;
+const FOLIAGE_COUNT = 8;
 const MAX_TREE_RADIUS = Math.PI / 6;
 const MIN_TREE_RADIUS = Math.PI / 11;
 
@@ -14,7 +19,7 @@ export function createPlanet(scene) {
     group.add(new THREE.Mesh(planetGeo, planetMat));
     group.position.set(60, -95, 80);
 
-    const campfireDir = new THREE.Vector3(2, 4.5, -4).normalize();
+    const campfireDir = new THREE.Vector3(2.6, 4.5, -4.5).normalize();
     const { campfire, flame, innerFlame, light } = makeCampfire();
     campfire.position.copy(campfireDir.clone().multiplyScalar(PLANET_RADIUS));
     campfire.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), campfireDir);
@@ -33,6 +38,15 @@ export function createPlanet(scene) {
         group.add(tree);
     }
 
+    for (let i = 0; i < FOLIAGE_COUNT; i++) {
+        const patch = makeFoliagePatch();
+        const randomTheta = Math.random() * Math.PI * 2;
+        const randomPhi = Math.random() * MAX_TREE_RADIUS + MIN_TREE_RADIUS;
+        const dir = new THREE.Vector3().copy(campfireDir).applyAxisAngle(new THREE.Vector3(0, 1, 0), randomPhi).applyAxisAngle(campfireDir, randomTheta).normalize();
+        patch.position.copy(dir.clone().multiplyScalar(PLANET_RADIUS));
+        patch.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), dir);
+        group.add(patch);
+    }
 
     scene.add(group);
 
@@ -42,18 +56,19 @@ export function createPlanet(scene) {
     function update(delta) {
         group.rotateOnAxis(campfireDir, delta * 0.07);
         time += delta;
+        const flicker = (1 + 0.25 * Math.sin(time * 9.3) + 0.15 * Math.sin(time * 14.7)) * 5;
 
         if (time < TITLE_FADE_START) {
             return;
         } else if (time < TITLE_FADE_END) {
-            const light_scale = (time - TITLE_FADE_START) / (TITLE_FADE_END - TITLE_FADE_START);
-            light.intensity = light_scale * 2.5;
-            light.distance = light_scale * 200;
+            const light_scale = ((time - TITLE_FADE_START) / (TITLE_FADE_END - TITLE_FADE_START)) * flicker;
+            light.intensity = light_scale;
+            light.distance = 200;
+        } else {
+            light.intensity = flicker;
+            flame.scale.set(1, 0.9 + 0.02 * flicker, 1);
+            innerFlame.scale.set(1, flicker * 0.2, 1);
         }
-        const flicker = 1 + 0.25 * Math.sin(time * 9.3) + 0.15 * Math.sin(time * 14.7);
-        light.intensity = 5 * flicker;
-        flame.scale.set(1, 0.9 + 0.1 * flicker, 1);
-        innerFlame.scale.set(1, flicker, 1);
     }
 
     return { update };
@@ -62,49 +77,45 @@ export function createPlanet(scene) {
 function makeCampfire() {
     const campfire = new THREE.Group();
 
-    const logMat = new THREE.MeshLambertMaterial({ color: 0x302216 });
-    const logGeo = new THREE.CylinderGeometry(0.2, 0.25, 4, 6);
-
-    const log1 = new THREE.Mesh(logGeo, logMat);
-    log1.rotation.z = Math.PI / 2;
-    log1.rotation.y = Math.PI / 4;
-    log1.position.y = 0.2;
-    campfire.add(log1);
-
-    const log2 = new THREE.Mesh(logGeo, logMat);
-    log2.rotation.z = Math.PI / 2;
-    log2.rotation.y = -Math.PI / 4;
-    log2.position.y = 0.2;
-    campfire.add(log2);
+    const rockMat = new THREE.MeshLambertMaterial({ color: 0x404040 });
+    for (let i = 0; i < ROCK_COUNT; i++) {
+        const rockGeo = new THREE.SphereGeometry(ROCK_SIZE + (Math.random() * ROCK_VARIANCE));
+        const rock = new THREE.Mesh(rockGeo, rockMat);
+        const theta = (i / ROCK_COUNT) * Math.PI * 2;
+        const x = Math.cos(theta);
+        const z = Math.sin(theta);
+        rock.position.set(x, 0.1, z);
+        campfire.add(rock);
+    }
 
     const flame = new THREE.Mesh(
-        new THREE.ConeGeometry(0.8, 1.8, 8),
+        new THREE.ConeGeometry(0.8, 1.8, 8, true),
         new THREE.MeshPhysicalMaterial({ color: 0xff6600 })
     );
     flame.position.y = 1;
     campfire.add(flame);
 
     const innerFlame = new THREE.Mesh(
-        new THREE.ConeGeometry(0.4, 1.4, 8),
+        new THREE.ConeGeometry(0.4, 1.4, 8, true),
         new THREE.MeshPhysicalMaterial({ color: 0xffdd00 })
     );
     innerFlame.position.y = 0.8;
     campfire.add(innerFlame);
 
     const light = new THREE.PointLight(0xffffff, 0, 0);
-    light.position.y = 3;
+    light.position.y = 2.5;
     campfire.add(light);
 
     return { campfire, flame, innerFlame, light };
 }
 
-const BASE_TREE_HEIGHT = 8;
-const TREE_HEIGHT_VARIANCE = 10;
+const BASE_TREE_HEIGHT = 10;
+const TREE_HEIGHT_VARIANCE = 16;
 
 function makePineTree() {
     const tree = new THREE.Group();
 
-    const trunkMat = new THREE.MeshLambertMaterial({ color: 0x2b1b16 });
+    const trunkMat = new THREE.MeshLambertMaterial({ color: 0x120c08 });
     const leafMat = new THREE.MeshLambertMaterial({ color: 0x2d5a27 });
     const treeHeight = BASE_TREE_HEIGHT + (Math.random() * TREE_HEIGHT_VARIANCE);
 
@@ -112,11 +123,34 @@ function makePineTree() {
     trunk.position.y = treeHeight / 2;
     tree.add(trunk);
 
-    for (const [radius, height, y] of [[3.2, 3.4, treeHeight - 2.8], [2.5, 3.2, treeHeight - 0.75], [1.8, 2.4, treeHeight + 0.5]]) {
+    for (const [radius, height, y] of [[3.2, 3.4, treeHeight - 2.8], [2.5, 2.8, treeHeight - 0.9], [1.8, 2.4, treeHeight + 0.5]]) {
         const cone = new THREE.Mesh(new THREE.ConeGeometry(radius, height, 8), leafMat);
         cone.position.y = y;
         tree.add(cone);
     }
 
     return tree;
+}
+
+function makeFoliagePatch() {
+    const patch = new THREE.Group();
+    const bladeCount = 5 + Math.floor(Math.random() * 5);
+    const colors = [0x4a7c35, 0x5a9040, 0x3d6b2e];
+
+    for (let i = 0; i < bladeCount; i++) {
+        const height = 1.2 + Math.random() * 1.5;
+        const geo = new THREE.ConeGeometry(0.25, height, 3);
+        const mat = new THREE.MeshLambertMaterial({ color: colors[Math.floor(Math.random() * colors.length)] });
+        const blade = new THREE.Mesh(geo, mat);
+        blade.position.set(
+            (Math.random() - 0.5) * 3,
+            height / 2,
+            (Math.random() - 0.5) * 3
+        );
+        blade.rotation.z = (Math.random() - 0.5) * 0.4;
+        blade.rotation.x = (Math.random() - 0.5) * 0.4;
+        patch.add(blade);
+    }
+
+    return patch;
 }
--- scripts/stars.js
+++ scripts/stars.js
@@ -6,30 +6,42 @@ const HEIGHT = window.innerHeight;
 
 let time = 0;
 
-const STAR_COUNT = (WIDTH + HEIGHT) / 1.5;
+const STAR_COUNT = (WIDTH + HEIGHT) * 2;
 const starData = [];
 const BASE_STAR_RADIUS = Math.min(1, WIDTH / 1300);
 const MIN_STAR_DISTANCE = 300;
 const MAX_STAR_DISTANCE = 400;
 
 // couldn't think of a better name for this, higher is slower; inverse spin factor?
-const STAR_SPIN_FACTOR = 20;
+const STAR_SPIN_FACTOR = 30;
 
 // supernova criteria
-const RED_GIANT_OFFSET = 300; // minimum time for stars to go supernova
-const RED_GIANT_SCALE = 3000; // max possible time after offset for supernova to happen
-const RED_GIANT_START_OFFSET = 120; // stars will start turning red over x seconds
-const SUPERNOVA_OFFSET = 20; // stars will go supernova x seconds after red giant has been reached
+const RED_GIANT_OFFSET = 20; // minimum time for stars to go supernova
+const RED_GIANT_SCALE = 20 // max possible time after offset for supernova to happen
+const RED_GIANT_START_OFFSET = 20; // stars will start turning red over x seconds
+const SUPERNOVA_OFFSET = 5; // stars will go supernova x seconds after red giant has been reached
+
+const STATUS_DEFAULT = "DEFAULT";
+const STATUS_REDMODE = "REDMODE";
+const STATUS_EXPLODED = "ESPLODED";
 
 // supernova color constants
 const SUPERNOVA_G = 0.75;
+
+const particleData = [];
+const PARTICLE_COUNT = 200;
+const PARTICLES_PER_EXPLOSION = 20;
 
 export function createStars(scene) {
     const starGeometry = new THREE.SphereGeometry(BASE_STAR_RADIUS, 16, 16);
     const starMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
     const instancedMesh = new THREE.InstancedMesh(starGeometry, starMaterial, STAR_COUNT);
 
+    const particleGeometry = new THREE.SphereGeometry(BASE_STAR_RADIUS / 5, 4, 4);
+    const particleMesh = new THREE.InstancedMesh(particleGeometry, starMaterial, PARTICLE_COUNT);
+
     const dummy = new THREE.Object3D();
+    const particleDummy = new THREE.Object3D();
     const position = new THREE.Vector3();
     const color = new THREE.Color();
 
@@ -47,20 +59,34 @@ export function createStars(scene) {
         dummy.updateMatrix();
 
         instancedMesh.setMatrixAt(i, dummy.matrix);
-        instancedMesh.material.transparent = true;
-        instancedMesh.material.opacity = 1;
         /**
          * size: max size of star (should oscillate between max and some smaller value)
          * opacity: also oscillating to simulate twinkling, can probly use this for size oscillation, too
          * midpoint: time when we should stop turning red and start turning blue
+         * status: current phase of the da moon :P
          */
         const midpoint = (Math.random() * RED_GIANT_SCALE) + RED_GIANT_OFFSET;
         starData.push({
             index: i,
             size: Math.random() * 2.5,
-            opacity: Math.random() * Math.PI,
+            opacity: Math.random() * Math.PI * 2,
             midpoint,
-            exploded: false,
+            brightness: 0,
+            status: STATUS_DEFAULT
+        });
+    }
+
+    let particlePointer = 0;
+    for (let i = 0; i < PARTICLE_COUNT; i++) {
+        // reserve 200 particles for explosions
+        // When supernova is reached, take the next 20 particles and send them in random directions
+        dummy.scale.setScalar(0);
+        dummy.updateMatrix();
+        particleMesh.setMatrixAt(i, dummy.matrix);
+        particleData.push({
+            index: i,
+            velocity: new THREE.Vector3(0, 0, 0),
+            size: 1
         });
     }
 
@@ -74,7 +100,6 @@ export function createStars(scene) {
             -Math.sin((delta / STAR_SPIN_FACTOR)), 0, Math.cos((delta / STAR_SPIN_FACTOR))
         );
         time += delta;
-        const toRemove = [];
         starData.forEach((star) => {
             instancedMesh.getMatrixAt(star.index, dummy.matrix);
             dummy.matrix.decompose(dummy.position, dummy.quaternion, dummy.scale);
@@ -84,27 +109,53 @@ export function createStars(scene) {
 
             if (time < START_FADE_IN_TIME) {
                 color.setHex(0x000000);
+                dummy.scale.setScalar(brightness * star.size * 0.5);
             } else if (time < END_FADE_IN_TIME) {
                 const scaled_brightness = (time - START_FADE_IN_TIME) * brightness;
                 color.setRGB(scaled_brightness, scaled_brightness, scaled_brightness);
+                dummy.scale.setScalar(brightness * star.size * 0.5);
             } else if (time < star.midpoint - RED_GIANT_START_OFFSET) {
                 color.setRGB(brightness, brightness, brightness);
+                dummy.scale.setScalar(brightness * star.size * 0.5);
             }
             else if (time < star.midpoint) {
+                // if first time switching to turning red, store current size and begin shrinking
+                if (star.status != STATUS_REDMODE) {
+                    star.status = STATUS_REDMODE;
+                    star.size = star.size * brightness * 0.5; // star.size now stores the max size of the star
+                    star.brightness = brightness; // and store brightness so that we can shift to red from this
+                }
                 const redness = (star.midpoint - time) / (RED_GIANT_START_OFFSET);
-                color.setRGB(brightness, redness * brightness, redness * brightness);
+                dummy.scale.setScalar(star.size * ((redness * 0.75) + 0.25));
+                color.setRGB(star.brightness, redness * star.brightness, redness * star.brightness);
             } else if (time < (star.midpoint + SUPERNOVA_OFFSET)) {
                 const redness = (star.midpoint + SUPERNOVA_OFFSET - time) / SUPERNOVA_OFFSET;
-                const r = brightness * redness;
-                const g = brightness * (1 - redness) * SUPERNOVA_G;
-                const b = brightness * (1 - redness);
+                const r = star.brightness * redness;
+                const g = star.brightness * (1 - redness) * SUPERNOVA_G;
+                const b = star.brightness * (1 - redness);
+                dummy.scale.setScalar((star.size * 0.25) + (1 - redness));
                 color.setRGB(r, g, b);
             } else {
-                if (!star.exploded) {
-                    star.exploded = true;
+                if (star.status != STATUS_EXPLODED) {
+                    star.status = STATUS_EXPLODED;
+                    dummy.scale.setScalar(0);
+                    // grab next n particles
+                    particleData
+                        .slice(particlePointer, particlePointer + PARTICLES_PER_EXPLOSION)
+                        .forEach((particle) => {
+                            particle.velocity = new THREE.Vector3(Math.random(), Math.random(), Math.random());
+                            particle.size = 1;
+                            // TODO: Make this work
+                            // particleDummy.matrix.decompose(particleDummy.position, particleDummy.quaternion, particleDummy.scale);
+                            // particleDummy.position = dummy.position.copy();
+                            // particleDummy.scale.setScalar(1);
+                            // particleMesh.setMatrixAt(particle.index, particleDummy);
+                        });
+                    particleMesh.instanceMatrix.needsUpdate = true;
+
+                    // move pointer
+                    particlePointer = (particlePointer + PARTICLES_PER_EXPLOSION) % PARTICLE_COUNT;
                 }
-                color.setRGB(0.1 * brightness, SUPERNOVA_G * brightness, brightness);
-                // play some particle effects and mark this for deletion
             }
 
 
@@ -113,14 +164,11 @@ export function createStars(scene) {
                 dummy.position.applyMatrix3(rotation_matrix);
             }
 
-            // derive scale from brightness
-            dummy.scale.setScalar(brightness * star.size * 0.5);
-
             dummy.updateMatrix();
+            instancedMesh.setColorAt(star.index, color)
             instancedMesh.setMatrixAt(star.index, dummy.matrix);
 
             //instancedMesh.scale.set(brightness * star.size, brightness * star.size, brightness * star.size);
-            instancedMesh.setColorAt(star.index, color)
 
         });
         instancedMesh.instanceColor.needsUpdate = true;
