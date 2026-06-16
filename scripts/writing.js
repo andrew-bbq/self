072daee5234b4838ca2d29827c47e73a22c89ccc
Andrew Bennett <awbennett@wisc.edu>
2026-06-16 16:54:04 -0700

    more spiral progress

--- scripts/writing.js
+++ scripts/writing.js
@@ -1,8 +1,14 @@
 const UNTRANSLATED = "<!> Untranslated Nomai writing <!>"
 const ABOUT_ME = "Andrew Bennett is a software engineer currently at Expedia Group."
 
-function writeAboutMe() {
-    const spiral = document.getElementById("about-me-spiral-1");
+function writeSpiral(id) {
+    const activeSpirals = document.getElementsByClassName("active-spiral");
+    [...activeSpirals].forEach((activeSpiral) => {
+        removeSpiralDrawing(activeSpiral);
+        activeSpiral.classList.remove("active-spiral");
+    });
+
+    const spiral = document.getElementById(id);
     spiral.classList.add("active-spiral");
     spiral.style.visibility = "visible";
     console.log(spiral.style.visibility);
@@ -13,3 +19,15 @@ function writeAboutMe() {
     glowPath.classList.remove("spiral-drawing");
     glowPath.classList.add("spiral-drawing");
 }
+
+function removeSpiralDrawing(spiral) {
+    const mainPath = spiral.querySelector(".nomai-writing-main-path");
+    const glowPath = spiral.querySelector(".nomai-writing-blur");
+    mainPath.classList.remove("spiral-drawing");
+    glowPath.classList.remove("spiral-drawing");
+}
+
+function writeSubSpiral(id) { }
+
+
+function translateSpiral(id) { }
