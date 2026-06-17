15f53f0710ecd3d5ab04310f72f74bb8b01eef98
Andrew Bennett <awbennett@wisc.edu>
2026-06-17 00:47:22 -0700

    basic translation logic

--- scripts/writing.js
+++ scripts/writing.js
@@ -1,5 +1,11 @@
-const UNTRANSLATED = "<!> Untranslated Nomai writing <!>"
-const ABOUT_ME = "Andrew Bennett is a software engineer currently at Expedia Group."
+const UNTRANSLATED = "<!> Untranslated Nomai writing <!>";
+
+const ID_TO_MESSAGE_MAP = {
+    "about-me-spiral-1": "Andrew Bennett is a software engineer!",
+    "education-spiral-1": "Andrew has a bachelor's degree in computer science and\na certificate in game design from the University of Wisconsin, Madison.\nI forget but I think his GPA was around 3.8 or give or take."
+}
+
+const translated = {};
 
 function writeSpiral(id) {
     const activeSpirals = document.getElementsByClassName("active-spiral");
@@ -7,7 +13,18 @@ function writeSpiral(id) {
         removeSpiralDrawing(activeSpiral);
         activeSpiral.classList.remove("active-spiral");
     });
+    writeSubSpiral(id);
+}
 
+function removeSpiralDrawing(spiral) {
+    const mainPath = spiral.querySelector(".nomai-writing-main-path");
+    const glowPath = spiral.querySelector(".nomai-writing-blur");
+    mainPath.classList.remove("spiral-drawing");
+    glowPath.classList.remove("spiral-drawing");
+}
+
+// write a spiral without closing other spirals
+function writeSubSpiral(id) {
     const spiral = document.getElementById(id);
     spiral.classList.add("active-spiral");
     spiral.style.visibility = "visible";
@@ -20,14 +37,16 @@ function writeSpiral(id) {
     glowPath.classList.add("spiral-drawing");
 }
 
-function removeSpiralDrawing(spiral) {
-    const mainPath = spiral.querySelector(".nomai-writing-main-path");
-    const glowPath = spiral.querySelector(".nomai-writing-blur");
-    mainPath.classList.remove("spiral-drawing");
-    glowPath.classList.remove("spiral-drawing");
+function translateSpiral(id) {
+    translated[id] = true;
+    showSpiralText(id);
 }
 
-function writeSubSpiral(id) { }
-
-
-function translateSpiral(id) { }
+function showSpiralText(id) {
+    const text = document.getElementById("translator-text");
+    if (!translated[id]) {
+        text.textContent = UNTRANSLATED;
+        return;
+    }
+    text.textContent = ID_TO_MESSAGE_MAP[id];
+}
